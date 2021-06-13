c2ae6e34e60628fb16394de145e90e0a94d5545d
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2021-06-13 17:50:30 +0800

    Initial Commit

---
+++ ShazamCustom/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShazamCustom
+//
+//  Created by Runhua Huang on 2021/6/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShazamCustom/ShazamCustomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamCustomApp.swift
+//  ShazamCustom
+//
+//  Created by Runhua Huang on 2021/6/13.
+//
+
+import SwiftUI
+
+@main
+struct ShazamCustomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
